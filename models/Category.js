f96c2e1dac93d595b609dabcbebfac4ef25caaa8
Prav Mukhi <praveshmukhi@gmail.com>
2021-06-27 14:08:55 -0400

    updated the models

---
+++ models/Category.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+// Model is standard from Sequelize and Category is an instance of the Model Class
+// Will inherit all the attributes of Model
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
